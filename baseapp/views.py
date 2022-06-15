5f76113f10acc2c366e6888a7141802402be8988
Продавец03 <apxujiy3@yandex.ru>
2022-06-15 17:55:40 +0200

    added bootstrap and learn styles css

--- baseapp/views.py
+++ baseapp/views.py
@@ -19,52 +19,52 @@ rules_list = ['Сложность зон и угроза противников'
 
 
 def index(request):
-    return render(request, 'index.html', {'title': title, 'main_menu': main_menu_list})
+    return render(request, 'baseapp/index.html', {'title': title, 'main_menu': main_menu_list})
 
 
 def about(request):
-    return render(request, 'main_menu/about_rp.html', {'title': title, 'main_menu': main_menu_list})
+    return render(request, 'baseapp/main_menu/about_rp.html', {'title': title, 'main_menu': main_menu_list})
 
 
 def enemies(request):
-    return render(request, 'main_menu/enemy_units_rp.html', {'title': title, 'main_menu': main_menu_list})
+    return render(request, 'baseapp/main_menu/enemy_units_rp.html', {'title': title, 'main_menu': main_menu_list})
 
 
 def rules(request):
-    return render(request, 'main_menu/main_rules_rp.html', {'title': title, 'main_menu': main_menu_list, 'rules': rules_list})
+    return render(request, 'baseapp/main_menu/main_rules_rp.html', {'title': title, 'main_menu': main_menu_list, 'rules': rules_list})
 
 
 def maps(request):
-    return render(request, 'main_menu/map_rp.html', {'title': title, 'main_menu': main_menu_list})
+    return render(request, 'baseapp/main_menu/map_rp.html', {'title': title, 'main_menu': main_menu_list})
 
 
 def schedule(request):
-    return render(request, 'main_menu/schedule_rp.html', {'title': title, 'main_menu': main_menu_list})
+    return render(request, 'baseapp/main_menu/schedule_rp.html', {'title': title, 'main_menu': main_menu_list})
 
 
 def battles(request):
-    return render(request, 'main_menu/rules/battles.html', {'title': title, 'main_menu': main_menu_list, 'rules': rules_list})
+    return render(request, 'baseapp/main_menu/rules/battles.html', {'title': title, 'main_menu': main_menu_list, 'rules': rules_list})
 
 
 def danger(request):
-    return render(request, 'main_menu/rules/danger_and_threat.html', {'title': title, 'main_menu': main_menu_list, 'rules': rules_list})
+    return render(request, 'baseapp/main_menu/rules/danger_and_threat.html', {'title': title, 'main_menu': main_menu_list, 'rules': rules_list})
 
 
 def death(request):
-    return render(request, 'main_menu/rules/HT_and_death.html', {'title': title, 'main_menu': main_menu_list, 'rules': rules_list})
+    return render(request, 'baseapp/main_menu/rules/HT_and_death.html', {'title': title, 'main_menu': main_menu_list, 'rules': rules_list})
 
 
 def movement(request):
-    return render(request, 'main_menu/rules/movement.html', {'title': title, 'main_menu': main_menu_list, 'rules': rules_list})
+    return render(request, 'baseapp/main_menu/rules/movement.html', {'title': title, 'main_menu': main_menu_list, 'rules': rules_list})
 
 
 def psionics(request):
-    return render(request, 'main_menu/rules/Psionics_and_warp.html', {'title': title, 'main_menu': main_menu_list, 'rules': rules_list})
+    return render(request, 'baseapp/main_menu/rules/Psionics_and_warp.html', {'title': title, 'main_menu': main_menu_list, 'rules': rules_list})
 
 
 def space_battles(request):
-    return render(request, 'main_menu/rules/space_battles.html', {'title': title, 'main_menu': main_menu_list, 'rules': rules_list})
+    return render(request, 'baseapp/main_menu/rules/space_battles.html', {'title': title, 'main_menu': main_menu_list, 'rules': rules_list})
 
 
 def economy(request):
-    return render(request, 'main_menu/rules/trade_and_economy.html', {'title': title, 'main_menu': main_menu_list, 'rules': rules_list})
+    return render(request, 'baseapp/main_menu/rules/trade_and_economy.html', {'title': title, 'main_menu': main_menu_list, 'rules': rules_list})
