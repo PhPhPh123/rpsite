8e2534f0a265f56c137ff3dae9c5c2becf333ec6
PC1 <sga1702@yandex.ru>
2022-06-17 17:47:55 +0200

    learning html and css, add main background, another will changed

--- baseapp/urls.py
+++ baseapp/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from baseapp.views import battles, danger, death, movement, psionics, space_battles, economy
 
 urlpatterns = [
-    path('battles/', battles),
-    path('danger/', danger),
-    path('death/', death),
-    path('movement/', movement),
-    path('psionics/', psionics),
-    path('spacebattles/', space_battles),
-    path('economy/', economy)
+    path('battles/', battles, name='home'),
+    path('danger/', danger, name='about'),
+    path('death/', death, name='death'),
+    path('movement/', movement, name='movement'),
+    path('psionics/', psionics, name='psionics'),
+    path('spacebattles/', space_battles, name='spacebattles'),
+    path('economy/', economy, name='economy')
 ]
