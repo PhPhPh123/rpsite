acd52abd8847d12b01bab9a489f504c6b7200d66
USer <sga1702@mail.ru>
2023-02-02 23:48:43 +0200

    send registration to email in progress1

--- rpsite/settings.py
+++ rpsite/settings.py
@@ -130,8 +130,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'apxujiy3@gmail.com'
-EMAIL_HOST_PASSWORD = 'tccofkshskqvytzw'
+EMAIL_HOST_USER = os.environ['GM_email']
+EMAIL_HOST_PASSWORD = os.environ['GM_password']
 
 
 
